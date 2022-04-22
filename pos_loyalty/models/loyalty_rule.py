e691614ead7a69b38bb16a41d9a21ee96e16b9ad
Anderson Buitron <andersonbuitron@unicauca.edu.co>
2022-04-22 10:02:22 -0500

    traduccion temporal de cadenas de texto a espanol - ya que no funcionaba la traduccion mediante los archivos .po

--- pos_loyalty/models/loyalty_rule.py
+++ pos_loyalty/models/loyalty_rule.py
@@ -9,38 +9,56 @@ from odoo import fields, models
 class LoyaltyRule(models.Model):
     _name = "loyalty.rule"
 
-    name = fields.Char(string="Rule Name", size=32, index=True, required=True)
+    #name = fields.Char(string="Rule Name", size=32, index=True, required=True)
+    name = fields.Char(string="Nombre de la regla", size=32, index=True, required=True)
     type = fields.Selection(
-        selection=[("product", "Product"), ("category", "Category")],
-        string="Type",
+        #selection=[("product", "Product"), ("category", "Category")],
+        #string="Type",
+        selection=[("product", "Producto"), ("category", "Categoria")],        
+        string="Tipo",
         required=True,
-        default="product",
-        help="The concept this rule applies to",
+        #default="product",
+        default="producto",
+        #help="The concept this rule applies to",
+        help="El concepto al que se aplica esta regla",
     )
     cumulative = fields.Boolean(
-        help="The points from this rule will be added "
-        "to points won from other rules with "
-        "the same concept"
+        string="Acumulativo",
+        help= "Los puntos de esta regla se sumarán a los puntos ganados de otras reglas con el mismo concepto.",
+        
+        #help="The points from this rule will be added "
+        #"to points won from other rules with "
+        #"the same concept" 
+        
+        
     )
     pp_product = fields.Float(
-        string="Points per product", help="Amount of points earned per product"
+        #string="Points per product", help="Amount of points earned per product"
+        string="Puntos por producto", help="Cantidad de puntos ganados por producto."
     )
     pp_currency = fields.Float(
-        string="Points per currency", help="Amount of points earned per currency"
+        #string="Points per currency", help="Amount of points earned per currency"
+        string="Moneda por punto", help="Cantidad de moneda vendida por punto ganado. \nEjemplo: 1000, es decir, por cada $1000 vendidos el cliente gana 1 punto."
     )
     loyalty_program_id = fields.Many2one(
         comodel_name="loyalty.program",
-        string="Loyalty Program",
-        help="The Loyalty Program this rule " "belongs to",
+        #string="Loyalty Program",
+        string="Programa de fidelidad",
+        #help="The Loyalty Program this rule " "belongs to",
+        help="El Programa de Lealtad esta regla pertenece a:",
     )
     product_id = fields.Many2one(
         comodel_name="product.product",
         domain=[("available_in_pos", "=", True)],
-        string="Target Product",
-        help="The product affected by this rule",
+        #string="Target Product",
+        #help="The product affected by this rule",
+        string="Producto objetivo",
+        help="El producto afectado por esta regla.",
     )
     category_id = fields.Many2one(
         comodel_name="pos.category",
-        string="Target Category",
-        help="The category affected by this rule",
+        #string="Target Category",
+        #help="The category affected by this rule",
+        string="Categoria objetivo",
+        help="La categoría afectada por esta regla.",
     )
