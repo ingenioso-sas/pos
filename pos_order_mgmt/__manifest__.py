c2b3ca01f58c8a3ba75bdc8e74761126019189c8
anderson buitron <andersonb@hotmail.es>
2026-05-29 00:44:28 -0500

    pos_order_mgmt: update translation template and spanish translations

--- pos_order_mgmt/__manifest__.py
+++ pos_order_mgmt/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "POS Frontend Orders Management",
     "summary": "Manage old POS Orders from the frontend",
-    "version": "13.0.1.4.0",
+    "version": "13.0.1.4.1",
     "category": "Point of Sale",
     "author": "GRAP, " "Tecnativa, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/pos",
